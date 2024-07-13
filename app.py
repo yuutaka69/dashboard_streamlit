52e2b8d85104c236e6e7ab7615afd11798d0d8a2
yuutaka69 <yuutaka3201996@yahoo.co.jp>
2024-07-13 16:22:00 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pandas as pd
 import os
 
+# Set Streamlit page configuration to wide mode
+st.set_page_config(layout="wide")
+
 # Streamlit app title
 st.title("Backtest Report")
 
